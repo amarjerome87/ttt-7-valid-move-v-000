8c1b514b2d510f2761bac002f14fa86c4776d674
Amar Chatman <amarjerome@gmail.com>
2017-11-13 23:39:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,7 @@
 def valid_move?(board, index)
-board[index] != position_taken?(board,index) 
+board[index] != position_taken?(board,index) &&  0<=board[index]>=8
 
 end
 
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
-end
